fc34356e281d91a83a6508d8cd170f3e6636134d
esp3j0 <107285370+esp3j0@users.noreply.github.com>
2026-03-27 14:24:32 +0800

    appfs: add huoyan http backend (#118)

--- examples/appfs/http-bridge/python/appfs_http_bridge/__init__.py
+++ examples/appfs/http-bridge/python/appfs_http_bridge/__init__.py
@@ -1,4 +1,5 @@
 from .fault_injector import DEFAULT_CONFIG_PATH, FaultInjector, FaultState
+from .huoyan_backend import HuoyanBackend
 from .jsonplaceholder_backend import JsonPlaceholderBackend
 from .mock_aiim import MockAiimBackend
 from .server import BridgeApplication, create_http_server, run_server
@@ -8,6 +9,7 @@ __all__ = [
     "DEFAULT_CONFIG_PATH",
     "FaultInjector",
     "FaultState",
+    "HuoyanBackend",
     "JsonPlaceholderBackend",
     "MockAiimBackend",
     "create_http_server",
